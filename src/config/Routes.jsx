4c07f4b9421549618d60b5873db3efaf7d2d83cb
annie burns <anniebnyc@gmail.com>
2016-10-08 16:21:22 -0400

    sql tables and init seed file added to migrations

---
+++ src/config/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from '../components/App.jsx';
+
+const Routes = () => (
+  <Router history={hashHistory} >
+    <Route path="/" component={App} />
+  </Router>
+);
+
+
+export default Routes;
