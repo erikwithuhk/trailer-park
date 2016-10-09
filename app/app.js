e63fdc55c4e1122b66929eb18dee366c21ef77dd
annie burns <anniebnyc@gmail.com>
2016-10-09 16:32:11 -0400

    added an api request to give us all the details we need in one

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const userRouter = require('./routes/userRouter.js');
+const request = require('superagent');
 
 const app = express();
+
+require('dotenv').config();
+process.env.ENV = process.env.ENV || 'dev';
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,5 +17,32 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 
 app.use('/api/users', userRouter);
+
+////ANNIE'S API CALLS BEGIN HERE///////////////////////////////////////////
+const apiKey = process.env.API;
+const movieId = 550;
+
+/// ONCE WE ALREADY HAVE THE movieId (above)
+// GET DETAILS BASED ON movieId
+app.get('/', (req, res) => {
+  request
+    .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=videos,credits`)
+    .catch((err) => {
+      res.send(err);
+    })
+    .then((movieDetails) => {
+      res.send(movieDetails.text)
+    });
+}),
+
+
+//NEXT WE NEED TO TAKE OUT THE RELEVANT DATA WE WANT (CLEAN IT UP):
+// original_title: Movie Title
+// genres:
+// id: Movie ID - need to store this
+// poster_path:
+// overview:
+// credits:Credits (include Director/Crew/Cast)
+
 
 module.exports = app;
