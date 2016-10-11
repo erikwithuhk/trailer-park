2a83ec74a9eb2879e8a9fe19b2a1980e14366bf4
Erik Jönsson <efjonsson@gmail.com>
2016-10-11 13:12:25 -0400

    style fixes

--- app/services/UserDAO.js
+++ app/services/UserDAO.js
@@ -4,19 +4,17 @@ const User = require('../models/User');
 
 class UserDAO {
   static all() {
-    return db.map(sql.all, [], (row) => new User(row));
+    return db.map(sql.all, [], row => new User(row));
   }
   static findBy(keyValue) {
     const key = Object.keys(keyValue)[0];
     const value = keyValue[key];
     return db.one(sql.find, [key, value])
-             .then((row) => {
-               return new User(row);
-             });
+             .then(row => new User(row));
   }
   static create({ email, password }) {
     return db.one(sql.create, [email, password])
-             .then((row) => new User(row));
+             .then(row => new User(row));
   }
   static delete(id) {
     return db.none(sql.delete, [id]);
