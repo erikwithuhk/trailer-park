6bdcbdc8de8b3a4e7f8a15c647b37ea48dc21c77
Erik Jönsson <efjonsson@gmail.com>
2016-10-11 14:25:00 -0400

    create delete controller

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -2,13 +2,15 @@ const UserDAO = require('../services/UserDAO');
 
 class UserController {
   static getAll(req, res) {
-    UserDAO.all().then((users) => {
-      res.status(200).json(users);
-    });
+    UserDAO.all()
+           .then(users => res.status(200).json(users));
   }
   static getOne(req, res) {
     UserDAO.findBy({ id: req.params.id })
-           .then((user) => res.status(200).json(user));
+           .then(user => res.status(200).json(user));
+  }
+  static deleteUser(req, res) {
+    res.status(200).send('Deleted');
   }
 }
 
--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -4,6 +4,8 @@ const UserController = require('../controllers/UserController');
 const router = express.Router();
 
 router.get('/', UserController.getAll);
+
 router.get('/:id', UserController.getOne);
+router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
