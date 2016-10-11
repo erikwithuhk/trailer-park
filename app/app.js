9a9fcbefae72a5fe176a7754d8c5fb0a7faa9c64
Erik Jönsson <efjonsson@gmail.com>
2016-10-11 09:36:18 -0400

    cleanup style errors

--- app/app.js
+++ app/app.js
@@ -22,7 +22,7 @@ app.use('/api/trailers', trailerRouter);
 const apiKey = process.env.API;
 const movieId = 550;
 
-/// ONCE WE ALREADY HAVE THE movieId (above)
+// ONCE WE ALREADY HAVE THE movieId (above)
 // GET DETAILS BASED ON movieId
 app.get('/api/trailers/:id', (req, res) => {
   request
@@ -31,10 +31,10 @@ app.get('/api/trailers/:id', (req, res) => {
       res.send(err);
     })
     .then((movieDetails) => {
-      res.send(movieDetails.text)
+      res.send(movieDetails.text);
     });
-}),
+});
 
-//NEXT WE NEED TO TAKE OUT THE RELEVANT DATA WE WANT (CLEAN IT UP):
+// NEXT WE NEED TO TAKE OUT THE RELEVANT DATA WE WANT (CLEAN IT UP):
 
 module.exports = app;
