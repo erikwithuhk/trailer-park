0fcb913166357703b0f4121d2d26b894ff56a429
Lynn Fleck <lynnfleck@me.com>
2016-10-11 15:13:45 -0400

    prepare for a merge

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import request from 'superagent';
+import UserForm from '../users/UserForm.jsx';
 
 const propTypes = {
   handleSubmit: React.PropTypes.func,
@@ -10,7 +11,7 @@ class UserProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
+      email: this.props.email,
       username: '',
       first_name: '',
       last_name: '',
@@ -18,31 +19,49 @@ class UserProfile extends Component {
       password: '',
     };
   }
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.handleSubmit(this.state);
-  }
   componentDidMount() {
-    // SUPERAGENT req to /api/users/:id
-    // need to grab user login info
-    // set state with logged in user's
-    this.setState({
-      email: 'grab the logged in user email',
-    });
+    this.getUserEmail();
   }
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <h1>My Profile</h1>
-          <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
-          <input type="text" onChange={this.handleChange} placeholder="User Name" />
-          <input type="text" name="first_name" placeholder="First Name" />
-          <input type="text" name="lastName" placeholder="Last Name" />
-          <input type="text" name="bio" placeholder="My Bio" />
-          <input type="submit" value="Update" />
+          <input
+            type="text"
+            name="email"
+            onChange={this.handleChange}
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            name="username"
+            onChange={this.handleChange}
+            placeholder="User Name"
+          />
+          <input
+            type="text" name="first_name"
+            onChange={this.handleChange}
+            placeholder="First Name"
+          />
+          <input
+            type="text"
+            name="lastName"
+            onChange={this.handleChange}
+            placeholder="Last Name"
+          />
+          <input
+            type="text"
+            name="bio"
+            onChange={this.handleChange}
+            placeholder="My Bio"
+          />
+          <input
+            type="submit"
+            value="Update"
+          />
         </form>
-        <button onClick={this.signOut} >Logout</button>
+        <button onClick={this.signOut}>Logout</button>
       </div>
     );
   }
