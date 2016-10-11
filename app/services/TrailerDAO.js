520ab206041f9421e4147904cc8958b5fb47c20b
Erik Jönsson <efjonsson@gmail.com>
2016-10-11 07:40:08 -0400

    fix style errors

--- app/services/TrailerDAO.js
+++ app/services/TrailerDAO.js
@@ -21,25 +21,27 @@ class TrailerDAO {
                 return this.getVideoKeys(searchResults);
               })
               .then(response => response)
+              // TODO return trailer objects
               .catch(err => err);
   }
   static getVideoKeys(trailers) {
-    const getVideoData = trailers.map((trailer) => {
-      return superagent.get(`https://api.themoviedb.org/3/${trailer.mediaType}/${trailer.tmdbID}/videos?api_key=${process.env.API_KEY}`)
+    const getVideoData = trailers.map(trailer =>
+      superagent.get(`https://api.themoviedb.org/3/${trailer.mediaType}/${trailer.tmdbID}/videos?api_key=${process.env.API_KEY}`)
                        .then((response) => {
+                         const trailerWithVideo = trailer;
                          const videoData = response.body.results;
                          if (videoData.length > 0) {
-                           trailer.hasTrailer = true;
-                           trailer.videoSite = videoData[0].site;
-                           trailer.videoKey = videoData[0].key;
+                           trailerWithVideo.hasTrailer = true;
+                           trailerWithVideo.videoSite = videoData[0].site;
+                           trailerWithVideo.videoKey = videoData[0].key;
                            // TODO search through videos returned and find trailers if present
-                           return trailer;
+                           return trailerWithVideo;
                          }
-                         trailer.hasTrailer = false;
-                         return trailer;
+                         trailerWithVideo.hasTrailer = false;
+                         return trailerWithVideo;
                        })
-                       .catch(err => err);
-    });
+                       .catch(err => err)
+    );
     return Promise.all(getVideoData).then(videosData => videosData);
   }
 }
