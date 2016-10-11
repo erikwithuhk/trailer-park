58dd2a919380c1c61d76ed620e38ff57354b7b2d
Lynn Fleck <lynnfleck@me.com>
2016-10-11 13:03:55 -0400

    set up input fields for user profiles

---
+++ client/components/UserProfile.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import request from 'superagent';
+
+const propTypes = {
+  handleSubmit: React.PropTypes.func,
+  handleInputChange: React.PropTypes.func,
+};
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      username: '',
+      first_name: '',
+      last_name: '',
+      bio: '',
+      password: '',
+    };
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleSubmit(this.state);
+  }
+  componentDidMount() {
+    // need to grab user login info
+    // set state with logged in user's
+    // this.setState({
+    //   email: ??
+    // })
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h1>My Profile</h1>
+          <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+          <input type="text" onChange={this.handleChange} placeholder="User Name" />
+          <input type="text" name="first_name" placeholder="First Name" />
+          <input type="text" name="lastName" placeholder="Last Name" />
+          <input type="text" name="bio" placeholder="My Bio" />
+          <input type="submit" value="Update" />
+        </form>
+        <button onClick={this.signOut} >Logout</button>
+      </div>
+    );
+  }
+}
+
+UserProfile.propTypes = propTypes;
+
+export default UserProfile;
--- client/config/Routes.jsx
+++ client/config/Routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from '../../client/components/App.jsx';
 import SearchTest from '../../client/components/SearchTest.jsx';
+import UserProfile from '../components/UserProfile.jsx';
 
 const Routes = () => (
   <Router history={hashHistory} >
     <Route path="/" component={App} />
     <Route path="/search-test" component={SearchTest} />
+    <Route path="/profile" component={UserProfile} />
   </Router>
 );
 
