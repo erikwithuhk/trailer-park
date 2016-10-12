2421b7bf64b6708be1532d102c2f9e6845264c6a
Erik Jönsson <efjonsson@gmail.com>
2016-10-12 11:23:21 -0400

    Revert "cleanup"
    This reverts commit 00051e3b9124d8dc38a40fff1ccb75d3a927a48c.

--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.get('/', UserController.getAll);
 router.get('/:user_id', UserController.getOne);
-router.get('/:user_id/trailer', UserController.getTrailers);
 router.patch('/:user_id', UserController.update);
 router.delete('/:user_id', UserController.delete);
 
