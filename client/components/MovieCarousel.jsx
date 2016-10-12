c588cf9ff9537060f4f3cca334141b42a8109847
Erik Jönsson <efjonsson@gmail.com>
2016-10-12 14:53:13 -0400

    cleanup, add trailers array to props validation

--- client/components/MovieCarousel.jsx
+++ client/components/MovieCarousel.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 
 const propTypes = {
+  trailers: React.PropTypes.array,
 };
 
-
-
 class MovieCarousel extends Component {
-
   render() {
     // const youTubeUrl = 'https://www.youtube.com/embed/';
     // const movieTrailerKey1 = 's7EdQ4FqbhY';
@@ -24,7 +22,7 @@ class MovieCarousel extends Component {
             <div className="broken-heart" />
           </li>
           <li className="items right-pos" id="2">
-            <img width="560" height="315" src="http://image.tmdb.org/t/p//w500//mte63qJaVnoxkkXbHkdFujBnBgd.jpg" />
+            <img width="560" height="315" src="http://image.tmdb.org/t/p//w500//mte63qJaVnoxkkXbHkdFujBnBgd.jpg" alt="" />
           </li>
           <li className="items back-pos" id="3">
             <iframe width="560" height="315" frameBorder="0" allowFullScreen />
@@ -39,13 +37,13 @@ class MovieCarousel extends Component {
             <iframe width="560" height="315" frameBorder="0" allowFullScreen />
           </li>
           <li className="items left-pos" id="7">
-            <img src="http://image.tmdb.org/t/p//w500/zkBN7dRpNiK4aaWF6c4WfecyXof.jpg" width="560" height="315" />
+            <img src="http://image.tmdb.org/t/p//w500/zkBN7dRpNiK4aaWF6c4WfecyXof.jpg" width="560" height="315" alt="" />
           </li>
         </ul>
-        // <span>
-        //   <input type="button" value="Prev" id="prev" />
-        //   <input type="button" value="Next" id="next" />
-        // </span>
+        <span>
+          <input type="button" value="Prev" id="prev" />
+          <input type="button" value="Next" id="next" />
+        </span>
       </section>
       );
   }
