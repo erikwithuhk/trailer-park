f9d7c224c9cd88cc2d7fc66178fddd21c2ae976d
Erik Jönsson <efjonsson@gmail.com>
2017-02-14 00:32:48 -0500

    return trailer data with user#find

--- app/db/sql/user/find.sql
+++ app/db/sql/user/find.sql
@@ -1,1 +1,4 @@
-SELECT * FROM users WHERE $1^=$2;
+SELECT users.id, email, username, first_name, last_name, bio, password, array_to_string(array_agg(CONCAT_WS('*@*', media_type, tmdb_id, title, blocked)), '#@#') AS trailers
+FROM users LEFT JOIN trailers ON users.id = trailers.users_id
+WHERE $1^=$2
+GROUP BY users.id;
