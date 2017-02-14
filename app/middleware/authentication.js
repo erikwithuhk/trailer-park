68a820307367cee03c540cd76c52d27138e840c9
Erik Jönsson <efjonsson@gmail.com>
2017-02-13 22:58:30 -0500

    fix linting errors

--- app/middleware/authentication.js
+++ app/middleware/authentication.js
@@ -3,21 +3,27 @@ const jwt = require('jsonwebtoken');
 function authenticate(req, res, next) {
   const { token } = req.cookies;
 
-  const nonRestrictedRoutes = ['/signup', '/login', '/signout', '/trailers', '/trailers/search', '/trailers/popular'];
+  const nonRestrictedRoutes = [
+    '/signup',
+    '/login',
+    '/signout',
+    '/trailers',
+    '/trailers/search',
+    '/trailers/popular',
+  ];
 
   if (nonRestrictedRoutes.includes(req.path)) { return next(); }
   if (token && req.session.currentUser) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         return res.status(403).end();
       }
       req.decoded = decoded;
-      next();
+      return next();
     });
-  } else {
-    res.clearCookie('token');
-    return res.status(403).end();
   }
+  res.clearCookie('token');
+  return res.status(403).end();
 }
 
 module.exports = authenticate;
