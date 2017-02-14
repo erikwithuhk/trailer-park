f26c17a8c0b285e08945da915017281894624202
Erik Jönsson <efjonsson@gmail.com>
2017-02-14 09:57:20 -0500

    return token from signup route

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -33,7 +33,7 @@ class AuthController {
             req.session.currentUser = user;
             const token = createToken(user);
             res.cookie('token', token);
-            res.status(200).json(user);
+            res.status(200).json(token);
           })
           .catch((err) => res.status(500).json(err));
     } else {
