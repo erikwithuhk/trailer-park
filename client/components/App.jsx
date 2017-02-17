e374577e1fa14db9e9e46062ea253b33bb065ffa
Erik Jönsson <efjonsson@gmail.com>
2017-02-16 20:12:48 -0500

    refactor app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -22,6 +22,24 @@ class App extends Component {
   componentDidMount() {
     this.updateAuth();
   }
+  createUserDisplayElement() {
+    if (this.state.currentUser) {
+      return (
+        <div className="top-nav_links">
+          <Link to="/search" >Search</Link>
+          <Link to="/community" >Community</Link>
+          <Link to="/profile" className="profile" >Profile</Link>
+          <Link to="#" onClick={this.signOut} >Sign out</Link>
+        </div>
+        );
+    }
+    return (
+      <div className="top-nav_links">
+        <Link to="/signup" className="signup" >Sign up</Link>
+        <Link to="/login" className="login" >Login</Link>
+      </div>
+    );
+  }
   getCurrentUser(token) {
     const decoded = jwtDecode(token);
     const { id, email, username, firstName, lastName, bio, trailers } = decoded;
@@ -31,9 +49,9 @@ class App extends Component {
   updateAuth() {
     const token = cookie.load('token');
     if (token) {
-      this.getCurrentUser(token);
+      return this.getCurrentUser(token);
     }
-    this.setState({ token });
+    return this.setState({ currentUser: null });
   }
   signUp(userDetails) {
     request.post('/api/signup')
@@ -72,27 +90,9 @@ class App extends Component {
            .catch(err => console.error(err));
   }
   render() {
-    let userDisplayElement;
-    if (this.state.currentUser) {
-      userDisplayElement = (
-        <div className="top-nav_links">
-          <Link to="/search" >Search</Link>
-          <Link to="/community" >Community</Link>
-          <Link to="/profile" className="profile" >Profile</Link>
-          <Link to="#" onClick={this.signOut} >Sign out</Link>
-        </div>
-        );
-    } else {
-      userDisplayElement = (
-        <div className="top-nav_links">
-          <Link to="/signup" className="signup" >Sign up</Link>
-          <Link to="/login" className="login" >Login</Link>
-        </div>
-      );
-    }
+    const userDisplayElement = this.createUserDisplayElement();
     const childrenWithProps = React.cloneElement(this.props.children, {
       currentUser: this.state.currentUser,
-      token: this.state.token,
       handleLogin: this.logIn,
       handleSignup: this.signUp,
       handleSignout: this.signOut,
