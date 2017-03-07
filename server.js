a645e1b9c1595e8d303f0d90ed1e6671bae60286
Erik Jönsson <efjonsson@gmail.com>
2017-03-06 23:09:40 -0500

    fix dev middleware capitalization

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-DEV-middleware');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const config = require('./webpack.config');
